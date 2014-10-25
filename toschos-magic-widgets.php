04191057e45769c2dd1e555923caaa5bad3d11ea
toscho <info@toscho.de>
2014-10-25 22:50:20 +0200

    Add widgets for log-in header and footer. Fixes #8

--- toschos-magic-widgets.php
+++ toschos-magic-widgets.php
@@ -76,8 +76,10 @@ class Toscho_Magic_Widgets
 		$actions = array (
 			'wp_head'       => __( 'Front End Header', 'plugin_magic_widgets' ),
 			'wp_footer'     => __( 'Front End Footer', 'plugin_magic_widgets' ),
-			'admin_head'    => __( 'Back End Header', 'plugin_magic_widgets' ),
-			'admin_footer'  => __( 'Back End Footer', 'plugin_magic_widgets' )
+			'admin_head'    => __( 'Back End Header',  'plugin_magic_widgets' ),
+			'admin_footer'  => __( 'Back End Footer',  'plugin_magic_widgets' ),
+			'login_head'    => __( 'Log-in Header',    'plugin_magic_widgets' ),
+			'login_footer'  => __( 'Log-in Footer',    'plugin_magic_widgets' ),
 		);
 
 		/**
